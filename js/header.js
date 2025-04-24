313d5c232e2304fd92b3d1efa6ac7b728c9ca4dc
Alyrw11 <alegab89@hotmail.co.uk>
2025-04-24 03:13:44 -0400

    trying to add the header to all pages

--- js/header.js
+++ js/header.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
     fetch("/html/header.html")
       .then(response => response.text())
       .then(data => {
-        document.getElementById("header-placeholder").innerHTML = data;
+        document.getElementById("headerPlaceholder").innerHTML = data;
       });
   });
   
